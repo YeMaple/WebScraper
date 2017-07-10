b3aceabc4a120b76f167e98db6cbf329669b7183
jerryfan4 <jerry.fan1996@gmail.com>
2017-07-10 11:25:13 -0700

    Authenticate with handler

--- practice/authenticate.py
+++ practice/authenticate.py
@@ -5,6 +5,9 @@ import re
 import base64
 from urlparse import urlparse
 
+
+# Authenticate manually
+"""
 url = 'https://bitbucket.org/dashboard/repositories'
 request = urllib2.Request(url)
 
@@ -48,4 +51,29 @@ except IOError, e:
     print e.reason
 
 read_page = response.read()
+print read_page
+"""
+
+# Authenticate with a basic handler
+password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+username = 'scraper4f'
+password = 'scraper123'
+
+top_url = 'https://bitbucket.org/'
+target_url = 'https://bitbucket.org/dashboard/repositories'
+
+# Add user setting to password manager
+password_mgr.add_password(None, top_url, username, password)
+
+# Create basic authentication handler
+auth_basic_handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+
+# Create & install opener
+opener = urllib2.build_opener(auth_basic_handler)
+urllib2.install_opener(opener)
+
+# Access the page
+response = urllib2.urlopen(target_url)
+read_page = response.read()
+
 print read_page
